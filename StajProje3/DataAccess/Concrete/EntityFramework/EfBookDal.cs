937543ea27682fe5a192e53654f9d8a45978ade0
Alperen Bozkurt <alperenbozkurt970@gmail.com>
2021-07-13 10:24:49 +0300

    Kitap, Yazar İlişkili Kütüphane Sistemi
    Proje 2(Staj)

---
+++ StajProje3/DataAccess/Concrete/EntityFramework/EfBookDal.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+using Core.DataAccess.EntityFramework;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfBookDal: EfEntityRepositoryBase<Book,BookStoreContext>,IBookDal
+    {
+
+    }
+}
